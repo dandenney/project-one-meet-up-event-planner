23cac76877a5878644646d035f0846c96cc22615
Dan Denney <dd.subscriptions@gmail.com>
2016-07-25 21:25:41 -0400

    Add placeholder event as an object, will move to Firebase later

---
+++ js/events.js
@@ -0,0 +1,40 @@
+// Add a placeholder event //
+
+var eventPlaceholder = {
+  name: 'Nanodegree Alumni Bash',
+  location: 'Impasse de la Fidélité 4, 1000 Bruxelles, Belgium',
+  type: 'Web',
+  dateTime: 'July 30, 9pm-12am',
+  host: 'Dan Denney',
+  description: 'This is a placeholder event description.'
+}
+
+function createPlaceholder () {
+
+  // Add name
+  var placeholderName = document.querySelector('#placeholder-name');
+  placeholderName.innerHTML = eventPlaceholder.name;
+
+  // Add location
+  var placeholderLocation = document.querySelector('#placeholder-location');
+  placeholderLocation.innerHTML = eventPlaceholder.location;
+
+  // Add type
+  var placeholderType = document.querySelector('#placeholder-type');
+  placeholderType.innerHTML = eventPlaceholder.type;
+
+  // Add dateTime
+  var placeholderDateTime = document.querySelector('#placeholder-dateTime');
+  placeholderDateTime.innerHTML = eventPlaceholder.dateTime;
+
+  // Add host
+  var placeholderHost = document.querySelector('#placeholder-host');
+  placeholderHost.innerHTML = eventPlaceholder.host;
+
+  // Add description
+  var placeholderDescription = document.querySelector('#placeholder-description');
+  placeholderDescription.innerHTML = eventPlaceholder.description;
+
+}
+
+createPlaceholder();
