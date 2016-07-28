684cb7613aec230e9c94173dc9d718d12920dfd9
Dan Denney <dd.subscriptions@gmail.com>
2016-07-27 21:28:45 -0400

    Clean up and add structure to validation.js

--- dist/js/all.js
+++ dist/js/all.js
@@ -64,78 +64,113 @@ function createPlaceholder () {
 
 createPlaceholder();
 
-// // Global Variables please work //
-var accountEmail = document.querySelector('#your-email');
-var firstPasswordInput = document.querySelector('#your-password');
-var accountCreate = document.querySelector('#account-create');
+// *************************************
+//
+//   Form Validation
+//   -> Validation and submission of forms
+//
+// *************************************
 
-// Check Requirements //
-function checkRequirements() {
+function validation() {
 
-  var firstPassword = firstPasswordInput.value;
-  var characterMessage = document.querySelector('.js-require-characters');
-  var capitalMessage = document.querySelector('.js-require-capital');
-  var numberMessage = document.querySelector('.js-require-number');
+  // -------------------------------------
+  //   Private Variables
+  // -------------------------------------
+  var accountEmail = document.querySelector('#your-email');
+  var accountPassword = document.querySelector('#your-password');
+  var accountCreate = document.querySelector('#account-create');
+  var accountForm = document.querySelector('.js-form-account');
 
-  // Check Password Length
-  if (firstPassword.length < 8) {
-    characterMessage.classList.remove('is-valid');
-    characterMessage.classList.add('is-invalid');
-  } else {
-    characterMessage.classList.remove('is-invalid');
-    characterMessage.classList.add('is-valid');
-  }
+  // -------------------------------------
+  //   Password Validation
+  // -------------------------------------
 
-  if (!firstPassword.match(/[A-Z]/g)) {
-    capitalMessage.classList.remove('is-valid');
-    capitalMessage.classList.add('is-invalid');
-  } else {
-    capitalMessage.classList.remove('is-invalid');
-    capitalMessage.classList.add('is-valid');
-  }
+  function checkPassword() {
 
-  if (!firstPassword.match(/\d/g)) {
-    numberMessage.classList.remove('is-valid');
-    numberMessage.classList.add('is-invalid');
-  } else {
-    numberMessage.classList.remove('is-invalid');
-    numberMessage.classList.add('is-valid');
-  }
+    // Private Variables
+    var firstPassword = accountPassword.value;
+    var characterMessage = document.querySelector('.js-require-characters');
+    var capitalMessage = document.querySelector('.js-require-capital');
+    var numberMessage = document.querySelector('.js-require-number');
 
-  console.clear();
+    // Apply validity classes for length
+    if (firstPassword.length < 8) {
+      characterMessage.classList.remove('is-valid');
+      characterMessage.classList.add('is-invalid');
+    } else {
+      characterMessage.classList.remove('is-invalid');
+      characterMessage.classList.add('is-valid');
+    }
 
-  if (firstPassword.length >= 8 && firstPassword.match(/[A-Z]/g) && firstPassword.match(/\d/g)) {
-    console.log('valid');
-  } else {
-    console.log('invalid');
-  }
+    // Apply validity classes for required capital letter
+    if (!firstPassword.match(/[A-Z]/g)) {
+      capitalMessage.classList.remove('is-valid');
+      capitalMessage.classList.add('is-invalid');
+    } else {
+      capitalMessage.classList.remove('is-invalid');
+      capitalMessage.classList.add('is-valid');
+    }
+
+    // Apply validity classes for required number
+    if (!firstPassword.match(/\d/g)) {
+      numberMessage.classList.remove('is-valid');
+      numberMessage.classList.add('is-invalid');
+    } else {
+      numberMessage.classList.remove('is-invalid');
+      numberMessage.classList.add('is-valid');
+    }
+
+  };
+
+  // -------------------------------------
+  //   Clear Feedback
+  // -------------------------------------
+
+  function clearFeedback() {
+    accountPassword.classList.remove('is-valid');
+    accountPassword.classList.remove('is-invalid');
+  };
+
+  // -------------------------------------
+  //   Account Submit
+  // -------------------------------------
+
+  function accountSubmit() {
+    event.preventDefault();
+    var email = accountEmail.value;
+    var password = accountPassword.value;
+    firebaseCreate(); // auth.js
+  };
+
+  // -------------------------------------
+  //   Event Listeners
+  // -------------------------------------
+
+  // Focus on account password
+  accountPassword.addEventListener('focus', function(event) {
+    clearFeedback();
+  }, true);
+
+  // Keyup on account password
+  accountPassword.addEventListener('keyup', function(event) {
+    checkPassword();
+  }, true);
+
+  // Submit of account form
+  accountForm.addEventListener('submit', function(event) {
+    accountSubmit();
+  }, true);
 
 };
 
-firstPasswordInput.addEventListener('focus', function(event) {
-  firstPasswordInput.classList.remove('is-valid');
-  firstPasswordInput.classList.remove('is-invalid');
-}, true);
+// -------------------------------------
+//   Initialize
+// -------------------------------------
 
-firstPasswordInput.addEventListener('keyup', function(event) {
-  checkRequirements();
-}, true);
+validation();
 
-var formAccount = document.querySelector('.js-form-account');
-formAccount.addEventListener('submit', function(event) {
-  event.preventDefault();
-  var email = accountEmail.value;
-  var password = firstPasswordInput.value;
-  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
-  });
-}, true);
-
-// Initialize on Submit
-
-accountCreate.onclick = function () {
-  checkRequirements();
-};
+// if (firstPassword.length >= 8 && firstPassword.match(/[A-Z]/g) && firstPassword.match(/\d/g)) {
+//   console.log('valid');
+// } else {
+//   console.log('invalid');
+// }
